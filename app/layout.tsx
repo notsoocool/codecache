aaf9b76ec744c173ae933f50334d5e80815f96af
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-02 13:45:30 +0530

    added:: Meta Tags for SEO Improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,30 @@ import type { Metadata } from "next";
 import "./styles/globals.css";
 import { ThemeProvider } from "@/components/global/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-
 import { ReactLenis } from "@/utils/lenis";
 
 export const metadata: Metadata = {
-	title: "CodeCache",
-	description: "CodeCache, a snippet manager for developers.",
+	title: "CodeCache - Snippet Manager for Developers", 
+	description: "CodeCache is a powerful snippet manager designed to help developers organize, store, and access their code snippets efficiently.", 
+	keywords: ["CodeCache", "Snippet Manager", "Code Storage", "Developer Tools", "Code Organization"], 
+	openGraph: {
+		title: "CodeCache - Snippet Manager for Developers", 
+		description: "Organize, store, and access code snippets efficiently with CodeCache.", 
+		url: "https://www.codecache.tech", 
+		type: "website", 
+		images: [
+			{
+				url: "https://www.codecache.tech/logo.svg", 
+				width: 1200,
+				height: 630,
+				alt: "CodeCache Banner", 
+			},
+		],
+	},
+	robots: {
+		index: true,
+		follow: true, 
+	}, 
 };
 
 export default function RootLayout({
@@ -18,7 +36,7 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-                {/* <ReactLenis root> */}
+				{/* <ReactLenis root> */}
 				<body>
 					<ThemeProvider
 						attribute="class"
@@ -29,7 +47,7 @@ export default function RootLayout({
 						{children}
 					</ThemeProvider>
 				</body>
-              {/*   </ReactLenis> */}
+				{/* </ReactLenis> */}
 			</html>
 		</ClerkProvider>
 	);
