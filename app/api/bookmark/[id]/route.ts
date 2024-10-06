80038c8fb4eccb51ee77ae6d0103d9c41a6cefa0
Yajush Vyas <dryashhoney@gmail.com>
2024-10-06 17:37:59 +0530

    v2 feat and fixes gssoc (#41)
    - feat: Added dropdown for languages in add-snippet
    - feat: added meta tags
    - fix: copy redirection error:
    - feat: bookmark page
    - feat: dynamic resizing codebox
    - fix: snippet approve issue
    
    ---------
    
    Co-authored-by: suhassk-hash <sharmasuhas450@gmail.com>
    Co-authored-by: Shivam <143623476+myselfshivams@users.noreply.github.com>
    Co-authored-by: Janvi <janvichoudhary116@gmail.com>
    Co-authored-by: Vivek7038 <vivek888chavan@gmail.com>
    Co-authored-by: Janvi <127668005+inkerton@users.noreply.github.com>
    Co-authored-by: Zayed <115407231+ZayedShahcode@users.noreply.github.com>

--- app/api/bookmark/[id]/route.ts
+++ app/api/bookmark/[id]/route.ts
@@ -4,59 +4,93 @@ import dbConnect from "@/lib/db/connect";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function PATCH(
-	req: Request,
-	{ params }: { params: { id: string } }
+  req: Request,
+  { params }: { params: { id: string } }
 ) {
-	const { id } = params;
+  const { id } = params;
 
-	try {
-		const user = await currentUser();
-		const userId = user?.id;
+  try {
+    const user = await currentUser();
+    const userId = user?.id;
 
-		if (!userId) {
-			return NextResponse.json(
-				{ error: "User not authenticated" },
-				{ status: 401 }
-			);
-		}
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User not authenticated" },
+        { status: 401 }
+      );
+    }
 
-		await dbConnect();
+    await dbConnect();
 
-		// Find the snippet by ID
-		const snippet = await Snippet.findById(id);
+    // Find the snippet by ID
+    const snippet = await Snippet.findById(id);
 
-		if (!snippet) {
-			return NextResponse.json(
-				{ error: "Snippet not found" },
-				{ status: 404 }
-			);
-		}
+    if (!snippet) {
+      return NextResponse.json({ error: "Snippet not found" }, { status: 404 });
+    }
 
-		// Toggle bookmark
-		const isBookmarked = snippet.bookmarkedBy.includes(userId);
-		if (isBookmarked) {
-			// User is already bookmarked, remove userId
-			snippet.bookmarkedBy = snippet.bookmarkedBy.filter(
-				(id) => id !== userId
-			);
-		} else {
-			// User is not bookmarked, add userId
-			snippet.bookmarkedBy.push(userId);
-		}
+    // Toggle bookmark
+    const isBookmarked = snippet.bookmarkedBy.includes(userId);
+    if (isBookmarked) {
+      // User is already bookmarked, remove userId
+      snippet.bookmarkedBy = snippet.bookmarkedBy.filter((id) => id !== userId);
+    } else {
+      // User is not bookmarked, add userId
+      snippet.bookmarkedBy.push(userId);
+    }
 
-		// Save snippet with the updated bookmarkedBy array
-		const updatedSnippet = await Snippet.findByIdAndUpdate(
-			id,
-			{ bookmarkedBy: snippet.bookmarkedBy }, // Only update the bookmarkedBy field
-			{ new: true } // Return the modified document
-		);
+    // Save snippet with the updated bookmarkedBy array
+    const updatedSnippet = await Snippet.findByIdAndUpdate(
+      id,
+      { bookmarkedBy: snippet.bookmarkedBy }, // Only update the bookmarkedBy field
+      { new: true } // Return the modified document
+    );
 
-		return NextResponse.json(updatedSnippet, { status: 200 });
-	} catch (error) {
-		console.error("Error updating bookmark:", error);
-		return NextResponse.json(
-			{ error: "Error updating bookmark" },
-			{ status: 500 }
-		);
-	}
+    return NextResponse.json(updatedSnippet, { status: 200 });
+  } catch (error) {
+    console.error("Error updating bookmark:", error);
+    return NextResponse.json(
+      { error: "Error updating bookmark" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  console.log("first", id);
+
+  try {
+	const user = await currentUser();
+    const userId = user?.id;
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    await dbConnect();
+
+    const bookmarkedSnippets = await Snippet.find({ bookmarkedBy: userId });
+	console.log('first1', bookmarkedSnippets);
+	
+    if (!bookmarkedSnippets) {
+		return NextResponse.json({ error: "Snippet not found" }, { status: 404 });
+    }
+	
+	// Return the bookmarked snippets
+	return NextResponse.json(bookmarkedSnippets, { status: 200 });
+
+  } catch (error) {
+    console.error("Error fetching bookmarks:", error);
+    return NextResponse.json(
+      { error: "Error fetching bookmarks" },
+      { status: 500 }
+    );
+  }
 }
