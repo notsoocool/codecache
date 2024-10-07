69b8e86c9cb81a4fa6ba838c41a55a8a8992405f
Priyanshu Verma <priyanshuverma@outlook.in>
2024-10-07 15:55:40 +0530

    fix dulplicate function

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,16 +16,5 @@ export function autoGrow(ref: React.RefObject<HTMLTextAreaElement>) {
   }
 }
 
-//  Automatically adjusts the height of a textarea based on its content.
-
-export function autoGrow(ref: React.RefObject<HTMLTextAreaElement>) {
-  if (ref.current) {
-    const { current } = ref;
-    current.style.height = "auto";
-    current.style.height = `${current.scrollHeight}px`;
-  } else {
-    console.warn("autoGrow: ref is not set or current is null");
-  }
-}
 
 export const baseURL = process.env.NEXT_PUBLIC_URL || "http://localhost:3000"
