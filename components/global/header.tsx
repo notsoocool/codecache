063e20d93a5e161b90298f019e483b7f1d7044c1
Gaurav Bomble <114927759+Gauravtb2253@users.noreply.github.com>
2024-10-22 00:13:56 +0530

    feat: added search function (#131)
    * added search function
    
    * fix
    
    * changes done
    
    * changes done1
    
    * changes done
    
    * changed files
    
    * Fixed
    
    * minor tweeks
    
    ---------
    
    Co-authored-by: Yajush Vyas <dryashhoney@gmail.com>

--- components/global/header.tsx
+++ components/global/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
-import { Inbox, Loader2, Sun, User2 } from "lucide-react";
+import { Inbox, Loader2, Search, Sun, User2 } from "lucide-react";
 import { Navigation } from "./navigation";
 import { HeaderLogo } from "./header-logo";
 import { MoonIcon } from "@radix-ui/react-icons";
@@ -15,19 +15,63 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "../ui/input";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { SearchContext } from "@/SearchContext";
+import React from "react";
+import { Skeleton } from "../ui/skeleton";
 
 export const Header = () => {
-  const [hasUnread, setHasUnread] = useState(false); // Tracks if there are unread notifications
+  const [hasUnread, setHasUnread] = useState(false);
+  const { setSearchQuery, snippets } = useContext(SearchContext);
+  const [inputValue, setInputValue] = useState("");
+  const [filteredSnippets, setFilteredSnippets] = useState(snippets);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(-1);
+  const resultRefs = React.useRef<(HTMLAnchorElement | null)[]>([]);
 
-  // Poll the API every 10 seconds
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setIsLoading(true);
+
+    // Simulating an API call with setTimeout
+    setTimeout(() => {
+      const filtered = snippets.filter((snippet) =>
+        snippet.title.toLowerCase().includes(value.toLowerCase()),
+      );
+      setFilteredSnippets(value ? filtered : []);
+      setIsLoading(false);
+      setSelectedIndex(-1);
+    }, 300);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      setSelectedIndex((prevIndex) =>
+        prevIndex < filteredSnippets.length - 1 ? prevIndex + 1 : prevIndex,
+      );
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+      setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : -1));
+    } else if (e.key === "Enter" && selectedIndex >= 0) {
+      e.preventDefault();
+      resultRefs.current[selectedIndex]?.click();
+    }
+  };
+
+  React.useEffect(() => {
+    if (selectedIndex >= 0) {
+      resultRefs.current[selectedIndex]?.focus();
+    }
+  }, [selectedIndex]);
+
   useEffect(() => {
     const intervalId = setInterval(async () => {
       try {
         const response = await fetch("/api/notifications");
         if (response.ok) {
           const notifications = await response.json();
-          // Check if there are any unread notifications
           const unreadExists = notifications.some(
             (notification: { status: string }) =>
               notification.status === "unread",
@@ -37,36 +81,82 @@ export const Header = () => {
       } catch (error) {
         console.error("Error fetching notifications:", error);
       }
-    }, 10000); // Poll every 10 seconds
+    }, 10000);
 
-    // Clear the interval when the component is unmounted
     return () => clearInterval(intervalId);
   }, []);
+
   const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const results = snippets.filter((snippet) =>
+      snippet.title.toLowerCase().includes(inputValue.toLowerCase()),
+    );
+    setFilteredSnippets(results);
+  }, [inputValue, snippets]);
+
   return (
-    <header className="sticky top-0 left-0 right-0 bg-transparent bg-opacity-20 backdrop-blur-md px-4 py-4 lg:px-14 z-50">
+    <header className="sticky top-0 left-0 right-0 bg-transparent bg-opacity-20 backdrop-blur-md px-4 py-4 lg:px-14 z-40">
       <div className="mx-auto">
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center lg:gap-x-16">
             <HeaderLogo />
             <Navigation />
           </div>
-          <div className="flex justify-center">
-            <div className="relative w-full max-w-2xl">
-              <Input
-                type="text"
-                placeholder="Search snippets..."
-                className="pl-6 pr-4 py-2 w-full"
-              />
+          <div className="mx-5 flex justify-center w-full max-w-sm">
+            <div className=" relative w-full">
+              <div className="  relative">
+                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                <Input
+                  type="text"
+                  placeholder="Search snippets..."
+                  className="pl-10 pr-4 py-2 w-full"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  onKeyDown={handleKeyDown}
+                  aria-label="Search snippets"
+                  aria-autocomplete="list"
+                  aria-controls="search-results"
+                  aria-expanded={filteredSnippets.length > 0}
+                />
+              </div>
+              {inputValue && (isLoading || filteredSnippets.length > 0) && (
+                <div
+                  id="search-results"
+                  className="absolute bg-white dark:bg-gray-800 mt-2 z-50 w-full shadow-lg rounded-md max-h-60 overflow-y-auto border border-gray-200 dark:border-gray-700"
+                >
+                  {isLoading
+                    ? Array.from({ length: 3 }).map((_, index) => (
+                        <div key={index} className="p-2">
+                          <Skeleton className="h-6 w-full" />
+                        </div>
+                      ))
+                    : filteredSnippets.map((snippet, index) => (
+                        <Link
+                          key={snippet._id}
+                          href={`/snippets/${snippet._id}`}
+                          className={`block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors ${
+                            index === selectedIndex
+                              ? "bg-gray-100 dark:bg-gray-700"
+                              : ""
+                          }`}
+                          onClick={() => setInputValue("")}
+                          tabIndex={-1}
+                        >
+                          {snippet.title}
+                        </Link>
+                      ))}
+                </div>
+              )}
             </div>
           </div>
           <div className="flex flex-row-reverse gap-4 items-center">
             <Link href="/notifications">
               <Button variant="outline" size="icon">
                 {hasUnread ? (
-                  <Inbox className="h-[1.2rem] w-[1.2rem] text-blue-500" /> // Default icon when no unread notifications
+                  <Inbox className="h-[1.2rem] w-[1.2rem] text-blue-500" />
                 ) : (
-                  <Inbox className="h-[1.2rem] w-[1.2rem]" /> // Default icon when no unread notifications
+                  <Inbox className="h-[1.2rem] w-[1.2rem]" />
                 )}
               </Button>
             </Link>
@@ -90,7 +180,7 @@ export const Header = () => {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-            <div className=" pr-2 flex items-center">
+            <div className="pr-2 flex items-center">
               <ClerkLoaded>
                 <UserButton>
                   <UserButton.MenuItems>
