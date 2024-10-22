07bc707e578d6d2f604591f02e6540856180175f
Yajush Vyas <dryashhoney@gmail.com>
2024-10-22 15:24:53 +0530

    minor tweeks

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -7,6 +7,42 @@ export const Footer = () => {
   return (
     <footer className="w-full border-t bg-background">
       <div className="py-8 px-10">
+        <div className="flex justify-between sm:flex-row sm:items-center">
+          <div className="space-y-4">
+            <p className="font-semibold text-muted-foreground">Quick Links</p>
+            <div className="flex text-sm space-x-4 justify-center sm:justify-start text-muted-foreground">
+              <Link href="/" className="hover:underline ">
+                Home
+              </Link>
+              <Link href="/snippets" className="hover:underline">
+                Snippets
+              </Link>
+              <Link href="/addsnippets" className="hover:underline">
+                Add Snippets
+              </Link>
+              <Link href="/contributor" className="hover:underline">
+                Contributors
+              </Link>
+            </div>
+          </div>
+
+          <div className="mt-10 flex justify-between items-center">
+            <p className="text-center text-sm leading-loose text-muted-foreground">
+              © {new Date().getFullYear()} CodeCache. All Rights Reserved.
+            </p>
+
+            <Button
+              variant="ghost"
+              size="icon"
+              className=" text-muted-foreground hover:text-white transition duration-300"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              <span className="sr-only">Back to Top</span>↑
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="py-2 px-10 border-t">
         <div className="flex flex-col justify-between sm:flex-row sm:items-center">
           <p className="text-center text-sm leading-loose text-muted-foreground sm:text-left">
             Join us in building CodeCache with{" "}
@@ -50,43 +86,6 @@ export const Footer = () => {
 								<span className="sr-only">Twitter</span>
 							</Button>
 						</Link> */}
-          </div>
-        </div>
-      </div>
-
-      <div className="pb-7 px-10">
-        <div className="flex justify-between sm:flex-row sm:items-center">
-          <div className="space-y-4">
-            <p className="font-semibold text-muted-foreground">Quick Links</p>
-            <div className="flex space-x-4 justify-center sm:justify-start text-muted-foreground">
-              <Link href="/" className="hover:underline">
-                Home
-              </Link>
-              <Link href="/snippets" className="hover:underline">
-                Snippets
-              </Link>
-              <Link href="/addsnippets" className="hover:underline">
-                Add Snippets
-              </Link>
-              <Link href="/contributor" className="hover:underline">
-                Contributors
-              </Link>
-            </div>
-          </div>
-
-          <div className="mt-10 flex justify-between items-center">
-            <p className="text-center text-sm leading-loose text-muted-foreground">
-              © {new Date().getFullYear()} CodeCache. All Rights Reserved.
-            </p>
-
-            <Button
-              variant="ghost"
-              size="icon"
-              className="text-muted-foreground hover:text-white transition duration-300"
-              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            >
-              <span className="sr-only">Back to Top</span>↑
-            </Button>
           </div>
         </div>
       </div>
